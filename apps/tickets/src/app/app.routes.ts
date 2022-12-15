a473d0a0c5de80132a5bf66c819bc04fcff6726a
robin schoch <rschoch1995@gmail.com>
2022-12-15 09:48:38 +0100

    exercise 2

--- apps/tickets/src/app/app.routes.ts
+++ apps/tickets/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {AboutComponent} from './about/about.component';
 import {HomeComponent} from './home/home.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {loadRemoteModule} from '@angular-architects/module-federation';
+import {startsWith, WebComponentWrapper, WebComponentWrapperOptions} from "@angular-architects/module-federation-tools";
 
 export const APP_ROUTES: Routes = [
   {
@@ -48,6 +49,44 @@ export const APP_ROUTES: Routes = [
       {
         path: 'about',
         component: AboutComponent,
+      },
+      {
+        path: 'angular2',
+        component: WebComponentWrapper,
+        data: {
+          type: 'script',
+          remoteEntry:
+            'https://gray-pond-030798810.azurestaticapps.net/remoteEntry.js',
+          remoteName: 'angular2',
+          exposedModule: './web-components',
+          elementName: 'angular2-element',
+        } as WebComponentWrapperOptions,
+      },
+
+      {
+        path: 'react',
+        component: WebComponentWrapper,
+        data: {
+          type: 'script',
+          remoteEntry:
+            'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+          remoteName: 'react',
+          exposedModule: './web-components',
+          elementName: 'react-element',
+        } as WebComponentWrapperOptions,
+      },
+
+      {
+        matcher: startsWith('angular3'),
+        component: WebComponentWrapper,
+        data: {
+          type: 'script',
+          remoteEntry:
+            'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
+          remoteName: 'angular3',
+          exposedModule: './web-components',
+          elementName: 'angular3-element',
+        } as WebComponentWrapperOptions,
       },
 
       // This _needs_ to be the last route!!
